a211aab189af45f895aa3f5f7d1622d6f6cd01b5
Daniel-Barragan-Torres <daniel.bros@hotmail.com>
2025-03-22 02:26:45 -0600

    feat: :memo: New Estado seed added

---
+++ app/Filament/Resources/EstadoResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EstadoResource\Pages;
+use App\Filament\Resources\EstadoResource\RelationManagers;
+use App\Models\Estado;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EstadoResource extends Resource
+{
+    protected static ?string $model = Estado::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+               
+                    Forms\Components\TextInput::make('nombre')
+                        ->required()
+            ]);
+    }
+//php artisan make:filament-resource Estado --generate
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre')
+                //->searchable(),
+                ->formatStateUsing(fn ($state) => is_array($state) ? implode(', ', $state) : $state)
+                ->searchable(),
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEstados::route('/'),
+            'create' => Pages\CreateEstado::route('/create'),
+            'edit' => Pages\EditEstado::route('/{record}/edit'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,31 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the application's database.W
      */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+      //  User::factory()->create([
+        //    'name' => 'Test User',
+         //   'email' => 'test@example.com',
+        //]);
+        DB::table('users')->insert([
+            'name' => 'Rose',
+            'email' => 'rose@barragan.com',
+            'password' => Hash::make ('barragan')
+           
+           /* 'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make ('password')
+*/
         ]);
     }
 }
